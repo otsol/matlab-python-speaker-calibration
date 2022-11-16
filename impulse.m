b7fc0500a233de238ead0d412d2caaea57ee577b
kasperikaivola <98049597+kasperikaivola@users.noreply.github.com>
2022-11-16 02:48:40 +0200

    Add spectrum smoothing

--- impulse.m
+++ impulse.m
@@ -1,3 +1,10 @@
+%{
+ELEC-C5341 - SASP - Äänen ja Puheenkäsittely
+Projektityö: Kaiuttimien kalibrointi
+%}
+clear;
+clc;
+close all;
 %x = readcell("Nov 9 11_00_58_no_intro.txt");
 % x signal, Fs sampling frequency
 [x, Fs] = audioread("speaker-front-impulse.wav");                  
@@ -12,23 +19,28 @@ P1 = P2(1:L/2+1);     % Single-sided spectrum
 P1(2:end-1) = 2*P1(2:end-1);
 
 f = Fs*(0:(L/2))/L;
-subplot(2,1,1)
+subplot(3,1,1)
 plot(f,P1)
 title("Single-Sided Amplitude Spectrum of X(t)")
 xlabel("f (Hz)")
 ylabel("|P1(f)|")
 
-subplot(2,1,2)
+subplot(3,1,2)
 indexOf1000Hz = floor(length(P1)*(1000/(Fs/2)));
 normFactor = P1(indexOf1000Hz);
 P1 = P1/normFactor;
-X_OCT = smoothSpectrum(P1,f,3);
 loglog(f,P1) %plot amplitude spectrum
 title("Single-Sided Amplitude Spectrum of X(t), log")
 xlabel("f (Hz)")
 ylabel("|P1(f)|")
 xlim([10 21000])
-
+subplot(3,1,3)
+P1_OCT = smoothSpectrum(P1,f',30);
+loglog(f,P1_OCT) %plot amplitude spectrum
+title("Single-Sided Amplitude Spectrum of X(t), log, smoothed")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+xlim([10 21000])
 
 %y = cell2mat(x);
 %figure(1)
