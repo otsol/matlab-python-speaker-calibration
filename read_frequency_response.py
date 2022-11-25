66a687396349c7f6de3df4344860f75115b92eae
Otso Luukkanen <otso.luukkanen@aalto.fi>
2022-11-25 04:43:34 +0200

    Added comments to python script

--- read_frequency_response.py
+++ read_frequency_response.py
@@ -4,19 +4,22 @@ import os
 from typing import List
 import argparse
 import numpy as np
+
 import matplotlib.pyplot as plt
 
+# command line argument parser with help messages
 parser = argparse.ArgumentParser(description="Create Equalizer APO config from frequency"
                                              "response CSV")
 parser.add_argument('sampling_frequency', metavar="Fs", type=int,
                     help="Give sampling frequency Fs (float)")
 parser.add_argument("file_name", metavar="file", type=str, action="store",
                     help="Give file name of frequency response CSV file")
-
 args = parser.parse_args()
 
+# set sampling frequency from 2nd command line argument
 Fs = args.sampling_frequency
 
+# empty list for reading csv lines
 a: List[float] = []
 # open file
 script_path = os.path.dirname(os.path.abspath(__file__))
@@ -30,6 +33,7 @@ with open(os.path.join(script_path, args.file_name), newline='\n') as csvfile:
 
 # convert list to numpy array
 c = np.array(a, float)
+# create frequency vector for plots
 f = np.linspace(1.0, 20000.0, num=len(c))
 # plot and show
 plt.plot(c)
@@ -38,16 +42,17 @@ plt.figure(figsize=(8, 2), dpi=160)
 plt.plot(np.log10(f), c, color='green', linewidth=0.2)
 plt.show()
 
+
 # https://stackoverflow.com/questions/53345583/python-numpy-exponentially-spaced-samples-between-a-start-and-end-value
-# this is from Stackoverflow
-def logarithmicspace(start, stop, power, num):
-    start = np.power(start, 1/float(power))
-    stop = np.power(stop, 1/float(power))
-    return np.power(np.linspace(start, stop, num=num), power)
+# similar function in stack overflow, log instead of exponential
 
-
-# self-made helper function, returns floats (probably)
-def powspace2(start, stop, power, num) -> np.ndarray:
+# self-made helper function, returns numpy array of floats (probably)
+# creates exponential spacing
+# start first value of output array
+# end last value of output array
+# power exponential power that determinates spacing
+# num number of array elements in total
+def powspace(start, stop, power, num) -> np.ndarray:
     start = math.log(start, power)
     stop = math.log(stop, power)
     return np.power(power, np.linspace(start, stop, num=num))
@@ -56,8 +61,7 @@ def powspace2(start, stop, power, num) -> np.ndarray:
 # choose 32 points for creating an EQ profile
 start = 32.0
 end = 18000.0
-# eq_points = np.logspace(6, )
-eq_frequencies = powspace2(start, end, 2, 32)
+eq_frequencies = powspace(start, end, 2, 32)
 eq_frequencies = np.floor(eq_frequencies).astype(int)
 print(eq_frequencies)
 
@@ -65,7 +69,7 @@ start_index = int(int(len(c) / 2) * (start / 20000))
 end_index = int(int(len(c) / 2) * (end / 20000))
 print(f'Start:{start_index}, End:{end_index}')
 # eq_points = np.logspace(6, )
-eq_indexes = powspace2(start_index, end_index, 2, 32)
+eq_indexes = powspace(start_index, end_index, 2, 32)
 eq_indexes = np.floor(eq_indexes).astype(int)
 print(eq_indexes)
 plt.figure(figsize=(8, 2), dpi=160)
